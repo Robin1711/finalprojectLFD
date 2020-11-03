74d631bcdbd463b1a6863eea22bcb1b1645a603f
RobinSommer <robin.sommer@planet.nl>
2020-11-03 20:46:24 +0100

    updated svm.py

--- svm.py
+++ svm.py
@@ -6,53 +6,43 @@ on the binary task of political orientation of articles regarding COP meetings.
 """
 
 # Importing libraries
-
 from main import *
 
 import time
-import sys
-import json
-import re
-import string
 import argparse
 
-import numpy as np
-
 from sklearn.metrics import *
-from sklearn.svm import SVC, LinearSVC
+from sklearn.svm import SVC
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import cross_val_score
 from sklearn.feature_extraction.text import TfidfVectorizer
-
-from constants import NEWSPAPER_ORIENTATION
 
 
 def identity(x):
     return x
 
-
-def make_svm(Xtrain, Ytrain, Xtest, Ytest):
-
-    print("Preprocessing happening")
-    index = 1
-
-    for doc in Xtrain:
-        preprocess_text(doc)
+def prepare_data(articles):
+    # return [preprocess_text(article) for article in articles]
+    prepared = list()
+    for index, article in enumerate(articles):
         print(index, end="\r")
-        index += 1
+        preprocessed_article = preprocess_text(article)
+        prepared.append(preprocessed_article)
+    return prepared
 
-    for doc in Xtest:
-        preprocess_text(doc)
-        print(index, end="\r")
-        index += 1
+def svm(Xtrain, Ytrain, Xtest, Ytest, use_cross_validating=False):
+    print("Preprocessing..")
+    Xtrain = prepare_data(Xtrain)
+    Xtest = prepare_data(Xtest)
+    print("Done!")
 
+    print("Building model..")
     vec = TfidfVectorizer(preprocessor=identity, tokenizer=identity)
-
     svc = SVC(kernel='rbf', C=100, gamma=0.01)
-
     classifier = make_pipeline(vec, svc)
+    print("Done!")
 
-    # Fitting our classifier onto our data
+    # Fitting our classifier onto our training data
     t0 = time.time()
     classifier.fit(Xtrain, Ytrain)
     train_time = time.time() - t0
@@ -73,11 +63,13 @@ def make_svm(Xtrain, Ytrain, Xtest, Ytest):
     print(classification_report(Ytest, Yguess))
     print(confusion_matrix(Ytest, Yguess))
 
-    # Defining 5 - fold cross - validation
-    print("Performing 5-fold cross validation..")
-    cv_classifier = make_pipeline(vec, svc)
-    scores = cross_val_score(cv_classifier, Xtrain, Ytrain, cv=5)
-    print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+    if (use_cross_validating):
+        # Defining 5 - fold cross - validation
+        print("Performing 5-fold cross validation..")
+        cv_classifier = make_pipeline(vec, svc)
+        scores = cross_val_score(cv_classifier, Xtrain, Ytrain, cv=5)
+        print("Done! Results:")
+        print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
 
 # If the script is run directly from the command line this is executed:
@@ -93,9 +85,9 @@ if __name__ == '__main__':
             f'TOTAL TRAINING INSTANCES = {len(Xtrain)},{len(Ytrain)}')
         print(
             f'TOTAL TESTING INSTANCES = {len(Xtest)},{len(Ytest)}\n')
-        make_svm(Xtrain, Ytrain, Xtest, Ytest)
+        svm(Xtrain, Ytrain, Xtest, Ytest)
     else:
-        X, Y = get_train_data()
+        X, Y = get_train_data([1,2,3,4])
         split_point = int(0.80 * len(X))
         Xtrain = X[:split_point]
         Ytrain = Y[:split_point]
@@ -105,4 +97,4 @@ if __name__ == '__main__':
             f'TOTAL TRAINING INSTANCES = {len(Xtrain)},{len(Ytrain)}')
         print(
             f'TOTAL TESTING INSTANCES = {len(Xtest)},{len(Ytest)}\n')
-        make_svm(Xtrain, Ytrain, Xtest, Ytest)
+        svm(Xtrain, Ytrain, Xtest, Ytest, use_cross_validating=True)
